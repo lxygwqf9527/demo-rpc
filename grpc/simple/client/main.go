dfc403d795a820001c1136221190c7fe7beef9d0
Your Name <you@example.com>
2022-11-09 23:04:11 +0800

    09/11/2022 3

--- grpc/simple/client/main.go
+++ grpc/simple/client/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/lxygwqf9527/demo-rpc/grpc/simple/server/pb"
 	"google.golang.org/grpc"
@@ -15,10 +16,40 @@ func main() {
 		panic(err)
 	}
 	client := pb.NewHelloServiceClient(conn)
+
+	// req <--> resp
 	resp, err := client.Hello(context.Background(), &pb.Request{Value: "alice"})
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println(resp)
+
+	// stream
+	stream, err := client.Channel(context.Background())
+	if err != nil {
+		panic(err)
+	}
+	// 启动一个Goroutine来发送请求
+	go func() {
+		for {
+			err := stream.Send(&pb.Request{Value: "alice"})
+			if err != nil {
+				panic(err)
+			}
+			time.Sleep(3 * time.Second)
+		}
+
+	}()
+
+	for {
+		// 主循环 负责接受响应
+		resp, err = stream.Recv()
+
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println(resp)
+	}
 
 }
